f8b3b393bf0c42b58126de610c747080d9e260a7
Daniel Rocha <daniel@rocha.io>
2022-11-15 21:26:00 +0100

    refactor: add `PSH_` prefix to constants

--- power.sh
+++ power.sh
@@ -4,56 +4,56 @@
 # Constants
 
 # External commands
-GIT_COMMAND="env LANG=C git"
+PSH_GIT_COMMAND="env LANG=C git"
 
 # Max number of path segments
-MAX_PATH_DEPTH=4
+PSH_MAX_PATH_DEPTH=4
 
 # Symbols
-SYMBOL_GIT_UNTRACKED='+'
-SYMBOL_GIT_AHEAD='↑'
-SYMBOL_GIT_BEHIND='↓'
+PSH_SYMBOL_GIT_UNTRACKED='+'
+PSH_SYMBOL_GIT_AHEAD='↑'
+PSH_SYMBOL_GIT_BEHIND='↓'
 
-SYMBOL_PATH_SEPARATOR='❯'
-SYMBOL_SSH='SSH'
-SYMBOL_ELLIPSIS='…'
+PSH_SYMBOL_PATH_SEPARATOR='❯'
+PSH_SYMBOL_SSH='SSH'
+PSH_SYMBOL_ELLIPSIS='…'
 
 # Color scheme
-COLOR_SSH_BG=166
-COLOR_SSH_FG=254
+PSH_COLOR_SSH_BG=166
+PSH_COLOR_SSH_FG=254
 
-COLOR_JOBS_BG=238
-COLOR_JOBS_FG=39
+PSH_COLOR_JOBS_BG=238
+PSH_COLOR_JOBS_FG=39
 
-COLOR_REPO_CLEAN_BG=148
-COLOR_REPO_CLEAN_FG=0
-COLOR_REPO_DIRTY_BG=161
-COLOR_REPO_DIRTY_FG=15
+PSH_COLOR_REPO_CLEAN_BG=148
+PSH_COLOR_REPO_CLEAN_FG=0
+PSH_COLOR_REPO_DIRTY_BG=161
+PSH_COLOR_REPO_DIRTY_FG=15
 
-COLOR_USERNAME_ROOT_BG=124
-COLOR_USERNAME_ROOT_FG=250
-COLOR_USERNAME_BG=240
-COLOR_USERNAME_FG=250
+PSH_COLOR_USERNAME_ROOT_BG=124
+PSH_COLOR_USERNAME_ROOT_FG=250
+PSH_COLOR_USERNAME_BG=240
+PSH_COLOR_USERNAME_FG=250
 
-COLOR_HOSTNAME_FG=250
-COLOR_HOSTNAME_BG=238
+PSH_COLOR_HOSTNAME_FG=250
+PSH_COLOR_HOSTNAME_BG=238
 
-COLOR_PATH_BG=237
-COLOR_PATH_FG=250
-COLOR_PATH_CWD=254
-COLOR_PATH_SEPARATOR=244
+PSH_COLOR_PATH_BG=237
+PSH_COLOR_PATH_FG=250
+PSH_COLOR_PATH_CWD=254
+PSH_COLOR_PATH_SEPARATOR=244
 
-COLOR_HOME_BG=31
-COLOR_HOME_FG=15
+PSH_COLOR_HOME_BG=31
+PSH_COLOR_HOME_FG=15
 
-COLOR_READONLY_BG=124
-COLOR_READONLY_FG=254
-COLOR_READONLY_SEPARATOR=248
+PSH_COLOR_READONLY_BG=124
+PSH_COLOR_READONLY_FG=254
+PSH_COLOR_READONLY_SEPARATOR=248
 
-COLOR_CMD_PASSED_BG=236
-COLOR_CMD_PASSED_FG=15
-COLOR_CMD_FAILED_BG=161
-COLOR_CMD_FAILED_FG=15
+PSH_COLOR_CMD_PASSED_BG=236
+PSH_COLOR_CMD_PASSED_FG=15
+PSH_COLOR_CMD_FAILED_BG=161
+PSH_COLOR_CMD_FAILED_FG=15
 
 # -----------------------------------------------------------------------------
 # Private functions
@@ -79,40 +79,42 @@ __psh_apply_color() {
 __psh_build_seg_git() {
     # Exit if Git isn't installed or current dir isn't a repo
     hash git &> /dev/null || return
-    $GIT_COMMAND rev-parse &> /dev/null || return
+    $PSH_GIT_COMMAND rev-parse &> /dev/null || return
 
     # Check if there are modifications on current branch
     local dirty count marks
-    dirty=$($GIT_COMMAND status --porcelain 2> /dev/null)
+    dirty=$($PSH_GIT_COMMAND status --porcelain 2> /dev/null)
     count=$(grep -c '^?? ' <<< "$dirty")
     marks=""
     if [[ $count -gt 0 ]]; then
-        marks+=" $SYMBOL_GIT_UNTRACKED$count"
+        marks+=" $PSH_SYMBOL_GIT_UNTRACKED$count"
     fi
 
     # Count number of revisions ahead or behind origin
     local status
-    status=$($GIT_COMMAND status --porcelain --branch 2> /dev/null)
+    status=$($PSH_GIT_COMMAND status --porcelain --branch 2> /dev/null)
     if [[ $status =~ ahead\ ([0-9]+) ]]; then
-        marks+=" $SYMBOL_GIT_AHEAD${BASH_REMATCH[1]}"
+        marks+=" $PSH_SYMBOL_GIT_AHEAD${BASH_REMATCH[1]}"
     fi
     if [[ $status =~ behind\ ([0-9]+) ]]; then
-        marks+=" $SYMBOL_GIT_BEHIND${BASH_REMATCH[1]}"
+        marks+=" $PSH_SYMBOL_GIT_BEHIND${BASH_REMATCH[1]}"
     fi
 
     local bg_color fg_color
     if [[ $dirty ]]; then
-        bg_color=$COLOR_REPO_DIRTY_BG
-        fg_color=$COLOR_REPO_DIRTY_FG
+        bg_color=$PSH_COLOR_REPO_DIRTY_BG
+        fg_color=$PSH_COLOR_REPO_DIRTY_FG
     else
-        bg_color=$COLOR_REPO_CLEAN_BG
-        fg_color=$COLOR_REPO_CLEAN_FG
+        bg_color=$PSH_COLOR_REPO_CLEAN_BG
+        fg_color=$PSH_COLOR_REPO_CLEAN_FG
     fi
 
     # Get current branch name or hash
     local branch
-    branch=$($GIT_COMMAND symbolic-ref HEAD 2> /dev/null || \
-             $GIT_COMMAND describe --tags --always 2> /dev/null)
+    branch=$(                                                  \
+        $PSH_GIT_COMMAND symbolic-ref HEAD 2> /dev/null ||     \
+        $PSH_GIT_COMMAND describe --tags --always 2> /dev/null \
+    )
 
     __psh_apply_color " ${branch#refs/heads/}${marks} " $fg_color $bg_color
 }
@@ -126,7 +128,7 @@ __psh_build_seg_jobs() {
     local count
     count=$(jobs | wc -l | xargs)
     if [[ $count -gt 0 ]]; then
-        __psh_apply_color " $count " $COLOR_JOBS_FG $COLOR_JOBS_BG
+        __psh_apply_color " $count " $PSH_COLOR_JOBS_FG $PSH_COLOR_JOBS_BG
     fi
 }
 
@@ -138,11 +140,11 @@ __psh_build_seg_jobs() {
 __psh_build_seg_username() {
     local bg_color fg_color
     if id -G | grep -qE '\<(544|0)\>'; then
-        bg_color=$COLOR_USERNAME_ROOT_BG
-        fg_color=$COLOR_USERNAME_ROOT_FG
+        bg_color=$PSH_COLOR_USERNAME_ROOT_BG
+        fg_color=$PSH_COLOR_USERNAME_ROOT_FG
     else
-        bg_color=$COLOR_USERNAME_BG
-        fg_color=$COLOR_USERNAME_FG
+        bg_color=$PSH_COLOR_USERNAME_BG
+        fg_color=$PSH_COLOR_USERNAME_FG
     fi
     __psh_apply_color ' \u ' $fg_color $bg_color
 }
@@ -153,7 +155,7 @@ __psh_build_seg_username() {
 #    None                                                                     #
 # --------------------------------------------------------------------------- #
 __psh_build_seg_hostname() {
-    __psh_apply_color ' \h ' $COLOR_HOSTNAME_FG $COLOR_HOSTNAME_BG
+    __psh_apply_color ' \h ' $PSH_COLOR_HOSTNAME_FG $PSH_COLOR_HOSTNAME_BG
 }
 
 # --------------------------------------------------------------------------- #
@@ -164,11 +166,11 @@ __psh_build_seg_hostname() {
 __psh_build_seg_ps() {
     local bg_color fg_color
     if [[ $1 -eq 0 ]]; then
-        bg_color=$COLOR_CMD_PASSED_BG
-        fg_color=$COLOR_CMD_PASSED_FG
+        bg_color=$PSH_COLOR_CMD_PASSED_BG
+        fg_color=$PSH_COLOR_CMD_PASSED_FG
     else
-        bg_color=$COLOR_CMD_FAILED_BG
-        fg_color=$COLOR_CMD_FAILED_FG
+        bg_color=$PSH_COLOR_CMD_FAILED_BG
+        fg_color=$PSH_COLOR_CMD_FAILED_FG
     fi
     __psh_apply_color ' \$ ' $fg_color $bg_color
 }
@@ -180,7 +182,7 @@ __psh_build_seg_ps() {
 # --------------------------------------------------------------------------- #
 __psh_build_seg_ssh() {
     if [[ $SSH_CLIENT ]]; then
-        __psh_apply_color " $SYMBOL_SSH " $COLOR_SSH_FG $COLOR_SSH_BG
+        __psh_apply_color " $PSH_SYMBOL_SSH " $PSH_COLOR_SSH_FG $PSH_COLOR_SSH_BG
     fi
 }
 
@@ -199,7 +201,7 @@ __psh_build_seg_path() {
         fi
         IFS='/' read -r -a folders <<< "${folders#'/'}"
     fi
-    local limit=$(( ${#folders[*]} - MAX_PATH_DEPTH ))
+    local limit=$(( ${#folders[*]} - PSH_MAX_PATH_DEPTH ))
 
     local separator
     for i in ${!folders[*]}; do
@@ -207,30 +209,30 @@ __psh_build_seg_path() {
         if [[ $i -eq 0 ]] || [[ $i -gt $limit ]]; then
             folder="${folders[$i]}"
         elif [[ $i -eq 1 ]]; then
-            folder="$SYMBOL_ELLIPSIS"
+            folder="$PSH_SYMBOL_ELLIPSIS"
         else
             continue
         fi
 
         local bg_color fg_color sp_color
         if [[ ! -w $PWD ]]; then
-            bg_color=$COLOR_READONLY_BG
-            fg_color=$COLOR_READONLY_FG
-            sp_color=$COLOR_READONLY_SEPARATOR
+            bg_color=$PSH_COLOR_READONLY_BG
+            fg_color=$PSH_COLOR_READONLY_FG
+            sp_color=$PSH_COLOR_READONLY_SEPARATOR
         elif [[ $PWD == "$HOME" ]]; then
-            bg_color=$COLOR_HOME_BG
-            fg_color=$COLOR_HOME_FG
+            bg_color=$PSH_COLOR_HOME_BG
+            fg_color=$PSH_COLOR_HOME_FG
         else
-            bg_color=$COLOR_PATH_BG
-            fg_color=$COLOR_PATH_FG
-            sp_color=$COLOR_PATH_SEPARATOR
+            bg_color=$PSH_COLOR_PATH_BG
+            fg_color=$PSH_COLOR_PATH_FG
+            sp_color=$PSH_COLOR_PATH_SEPARATOR
             if [[ $i -eq $(( ${#folders[*]} - 1 )) ]]; then
-                fg_color=$COLOR_PATH_CWD
+                fg_color=$PSH_COLOR_PATH_CWD
             fi
         fi
         __psh_apply_color "$separator" $sp_color
         __psh_apply_color " $folder " $fg_color $bg_color
-        separator=$SYMBOL_PATH_SEPARATOR
+        separator=$PSH_SYMBOL_PATH_SEPARATOR
     done
 }
 
